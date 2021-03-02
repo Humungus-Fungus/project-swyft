4edc7a58c40c9a18f47b26cb3c2d3248d0704eb3
Humungus Fungus <devarshdinesh@gmail.com>
2021-03-02 07:40:39 +0400

    Update to python interfaces
    Still need to add comments to the kv file

--- main.py
+++ main.py
@@ -1,73 +1,77 @@
-from kivy.app import App
-from kivy.lang import Builder
-import ColoredLabels
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.textinput import TextInput
-from kivy.uix.popup import Popup
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.image import Image
-from kivy.properties import ObjectProperty, NumericProperty, StringProperty
-from kivy_garden.mapview import MapView
-import re
+from kivy.app import App  # This imports the kivy library
+from kivy.lang import Builder  # This imports the builder, which is used to cmopile the kvylang code
+import ColoredLabels  # extends functionality to allow labels to have color
+from kivy.uix.screenmanager import ScreenManager, Screen  # allows the creation of screen and the manager for them
+from kivy.uix.textinput import TextInput  # allows input to be taken directly from the UI
+from kivy.uix.label import Label  # Allows creating of labels
+from kivy.uix.popup import Popup  # Allows creation of popups
+from kivy.uix.boxlayout import BoxLayout  # Allows the UI to be arranged using the layout known as 'box layout'
+from kivy.uix.floatlayout import FloatLayout  # Allows the UI to be arranged using the layout known as 'float layout'
+from kivy.uix.image import Image  # Allows images to be displayed in the UI
+from kivy.properties import * # import all properties, which are used for things like storing input from the UI
+from kivy_garden.mapview import MapView, MapMarkerPopup # Allows maps to be used, and markers to place on them
 
 
+# Represents home screen
 class HomeScreen(Screen):
-    destination_input = ObjectProperty()
-    destination = StringProperty('')
-    latitude = NumericProperty(0)
-    longitude = NumericProperty(0)
+    def __init__(self, **kwargs):
+        super(HomeScreen, self).__init__(**kwargs)  # Initialisation of the HomeScreen class
 
-    def enter_dest(self):
-        # Assuming the input is the exact coordinate
-        pattern = r"\((\d+), (\d+)\)"
-        matches = re.search(pattern, self.destination_input.text)
+        # The input for the latitude and longitude
+        self.lat_inp = TextInput(text="10", pos_hint={"center_x": 0.425, "top": 1}, size_hint=(0.25, 0.125), multiline=False)
+        self.lon_inp = TextInput(text="10", pos_hint={"center_x": 0.675, "top": 1}, size_hint=(0.25, 0.125), multiline=False)
 
-        try:
-            latitude, longitude = matches.group(1), matches.group(2)
-            print("lat: " + latitude)
-            print("lon: " + longitude)
-        except AttributeError:
-            # Assuming the input is the node number
-            self.destination = self.destination_input.text
-            print("Got the dest: {}".format(self.destination))
+        # Labels to store the latitude and longitude
+        self.lat_label = Label(text=self.lat_inp.text, pos_hint={"center_x": 0.425, "top": 0.875}, size_hint=(0.25, 0.125))
+        self.lon_label = Label(text=self.lat_inp.text, pos_hint={"center_x": 0.625, "top": 0.875}, size_hint=(0.25, 0.125))
+
+        # Adds the widgets previously declared
+        self.add_widget(self.lat_inp)
+        self.add_widget(self.lon_inp)
+        self.add_widget(self.lat_label)
+        self.add_widget(self.lon_label)
 
 
 # Represents the screen with the map
 class MapScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(MapScreen, self).__init__(**kwargs) # Initialisation
+        self.map_view = MapView(lat=10, lon=10, zoom=5) # Adds the map
+        self.var_lat = 0  # stores the latitude
+        self.var_lon = 0  # stored the longitude
 
-
-# Contents of the popup
-class PopUpDesign(FloatLayout):
-    pass
+    def update(self):  # Updates the variables to the new latitude and longitude values
+        self.var_lat = float(HomeScreen().lat_label.text)
+        self.var_lon = float(HomeScreen().lon_label.text)
+        print(str(self.var_lat) + " and " + str(self.var_lon))
 
 
 # Represents the screen with all settings
 class SettingsScreen(Screen):
-    home_loc = NumericProperty(0)
+    def __init__(self, **kwargs):
+        super(SettingsScreen, self).__init__(**kwargs)  # initialisation
 
-    def change_home_loc(self):
-        popup_contents = PopUpDesign()
-        self.popup = Popup(title="New Home Loc", contents=popup_contents, size_hint=(0.4, 0.4))
-        self.popup.open()
-
-    def close_popup(self):
-        self.popup.dismiss()
+        # This is the location set as the home address. Can be changed at any time
+        self.home_loc_inp = TextInput(pos_hint={"left": 1, "top": 0.8}, size_hint=(0.4, 0.1), text="New home loc",
+                                      multiline=False)
+        self.add_widget(self.home_loc_inp)  # Adds this widget
 
 
-# Manager all screens
+# Manages all screens
 class WinManager(ScreenManager):
     pass
 
 
-kv = Builder.load_file("customwidget.kv")
+kv = Builder.load_file("customwidget.kv")  # Builds all kivy code from the customwidgets.kv file
 
 
+# Class where the app is run
 class KivyApp(App):
 
     def build(self):
         return kv
 
 
-KivyApp().run()
+# runs the program
+if __name__ == "__main__":
+    KivyApp().run()
